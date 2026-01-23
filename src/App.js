9df53827ef1daa68ae710fee31f4ddbf8d8a0083
Aagam Shah <aagam.infynno@gmail.com>
2026-01-23 16:59:56 +0530

    Update App component: change placeholder text to "Testing apple developer merchantid domain association".

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Testing 123
+        Testing apple developer merchantid domain association
       </header>
     </div>
   );
